b45d388724ec02d298c7084af01d49c973c5b31e
Nik McLaughlin <72171383+nikmclaughlin@users.noreply.github.com>
2022-07-21 20:22:56 -0700

    Build basic UI. Add blog to README

--- README.md
+++ README.md
@@ -2,4 +2,6 @@
 
 **A stupidly simple app to answer a timeless question by way of chaotic consensus**
 
+Check out the dev blog for more: https://nikmclaughlin.com/blog
+
 ### Vite template from @KrishGarg https://github.com/KrishGarg/vite-react-tailwind-jit-template
--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,19 @@
 const App = () => {
+
   return (
-    <div className="font-bold text-4xl m-[2em] text-red-600 underline">
-      Hello World
-    </div>
-  );
+    <>
+      <div className="font-bold text-4xl p-[2em] mx-auto text-red-600">
+        How many licks does it take to get to the center of a tootsie pop?
+      </div>
+      <img className="max-w-sm mx-auto" src="https://www.pinclipart.com/picdir/big/567-5677775_freetoedit-tootsieroll-owl-how-many-licks-does-it.png"></img>
+      <div className="h-8"></div>
+      <div className="font-bold text-3xl p-4 text-center">9001</div>
+      <div className="container w-screen mx-auto flex space-x-48 justify-center">
+        <button className="text-3xl bg-slate-600 rounded-xl p-6">More Licks</button>
+        <button className="text-3xl bg-slate-600 rounded-xl p-6">Less Licks</button>
+      </div>
+    </>
+);
 };
 
 export default App;
